6e1c5b0fc6326a11ee362d5bc15a7d23a74fff96
hyorimm <hyorimm@gmail.com>
2022-10-04 19:17:59 +0900

    1st

---
+++ main.js
@@ -0,0 +1,14 @@
+// new Swiper(선택자, 옵션)
+new Swiper(".main_image .swiper-container", {
+  direction: "horizontal"
+  loop: true,
+  slidesPerView: 1, //한번에 보여줄 슬라이드 개수
+  pagination: {
+    el: ".main_image .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, //사용자의 페이지 번호 요소 제어 선택자
+  },
+  navigation: {
+    prevEl: ".main_image .swiper-prev",
+    nextEl: ".main_image .swiper-next",
+  },
+});
